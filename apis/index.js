74d93d6b77310931642e53d5b17200ffbdddb164
LN Quy <lnquy065@gmail.com>
2018-04-02 17:01:39 +0700

    Add Homepage

--- apis/index.js
+++ apis/index.js
@@ -1,10 +1,14 @@
 var express  = require('express');
 var router = express.Router();
+var path    = require("path");
 
 //middleware
 router.use('/api/coins', require('./coins'));
 router.use('/api/tokens', require('./tokens'));
 router.use('/api/accounts', require('./accounts'));
 
+router.use('/', (req, res) => {
+    res.sendFile('/views/homepage.html', {root: __dirname});
+})
 
 module.exports = router;
